00422e556d6da3d6aff0d65054eb1c73b42bbe7e
yurik1m <yuri.k.tech@gmail.com>
2023-08-08 19:53:05 +0900

    [Feat]bookmark 추가 및 삭제 api 추가

--- NurVo_Backend/controllers/bookmark.mjs
+++ NurVo_Backend/controllers/bookmark.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getDialogueOfBookemark } from '../services/bookmark.mjs';
+import { getDialogueOfBookemark, saveBookemark } from '../services/bookmark.mjs';
 
 export const router = express.Router();
 
 router.get('/', getBookmark);
+router.post('/', addBookmark);
+router.post('/delete', deleteBookmark);
 
 async function getBookmark(req, res) {
   try {
@@ -16,5 +18,26 @@ async function getBookmark(req, res) {
   } catch (err) {
     console.err(err);
     res.send({"message":"로드실패"});
+  };
+};
+
+async function addBookmark(req, res) {
+  try {
+    const conversation_id = req.body.conversation_id;
+    console.log(conversation_id);
+    await saveBookemark(conversation_id);
+    res.status(200).send({"message":"save"});
+  } catch (err) {
+    console.error(err);
+  };
+};
+
+async function deleteBookmark(req, res) {
+  try {
+    const conversationIds = req.body;
+    console.log(conversationIds);
+    res.send({"message":"delete"});
+  } catch (err) {
+    console.error(err);
   };
 };
