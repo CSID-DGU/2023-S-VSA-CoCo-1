015d722fc5444f2039ce33f36ef882847f98ae37
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-08-14 02:56:05 +0900

    [Feat] #6 - 날짜 선택 모달

---
+++ NurVo_Frontend/src/components/DateTimePickerModalProps.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import DateTimePicker from "react-native-modal-datetime-picker";
+
+interface DateTimePickerModalProps {
+  isAction: boolean,
+  onDate: (value: string) => void,
+  onDisable: (value: boolean) => void,
+  onAlertAction: (value: boolean) => void,
+}
+
+const DateTimePickerModal = ({ isAction, onDate, onDisable, onAlertAction }: DateTimePickerModalProps) => {
+  const [isDisable, setIsdisable] = useState(false);
+
+  useEffect(() => {
+    if (isAction) setIsdisable(true);
+  }, [isAction]);
+
+  const handleClosed = () => {
+    setIsdisable(false);
+    onDisable(false);
+  };
+
+  const handleNext = (pickedDate) => {
+    if (pickedDate > Date.now()) {
+      const year = pickedDate.getFullYear().toString();
+      const month = (pickedDate.getMonth() + 1).toString();
+      const day = pickedDate.getDate().toString();
+      onDate(`${year}. ${month}. ${day}.`);
+    } else {
+      onAlertAction(true);
+    }
+    handleClosed();
+  };
+
+  return (
+    <>
+      <DateTimePicker
+        isVisible={isDisable}
+        onConfirm={handleNext}
+        onCancel={handleClosed}
+      />
+    </>
+  );
+}
+
+export default DateTimePickerModal;
