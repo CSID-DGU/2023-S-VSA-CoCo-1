22662cf5673797889f81ed97eea2f5039b63376a
jeonjimin <cjjunn0827@gmail.com>
2023-08-17 04:13:06 +0900

    [Feat] #21 - 전체 학습 리스트 UI

--- NurVo_Frontend/src/pages/LessonsList.tsx
+++ NurVo_Frontend/src/pages/LessonsList.tsx
@@ -1,107 +1,105 @@
 import {
-    SectionList,
-    Text
+  SectionList,
+  View
 } from 'react-native';
 import { useEffect, useState } from 'react';
-import { RN_HOST_URL, TEST_TOKEN } from '@env';
 import { ListCell } from '../components/ListCellComp';
-import { Subtext011, Subtitle011, Title02 } from '../utilities/Fonts';
+import { Subtitle011 } from '../utilities/Fonts';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { stopSpeech } from '../utilities/TextToSpeech';
+import { LessonListProps } from '../utilities/NavigationTypes';
+import { fetchAllTopic } from '../utilities/ServerFunc';
 
 
 interface Topic {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
-interface Chapter {
-    id: number;
-    name: string;
-    description?: string;
-    topic_id: number;
+export interface Chapter {
+  id: number;
+  name: string;
+  description?: string;
+  topic_id: number;
 }
 
-interface Section {
-    topic: Topic;
-    chapter: Chapter[];
+export interface Section {
+  topic: Topic;
+  chapter: Chapter[];
 }
 
 interface ListData {
-    title: string;
-    data: Chapter[];
+  title: string;
+  data: Chapter[];
 }
 
 export interface ResponseProps {
-    data: string;
+  data: string;
 }
-//topic과 chpater 요청
-async function fetchAllTopic(): Promise<Section[] | undefined> {
-    const HOST_URL = RN_HOST_URL;
-    const url = `${HOST_URL}/api/dialogues`;
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + TEST_TOKEN
-            }
-        });
-        const responseData = await response.json();
-        return responseData;
-    } catch (error) {
-        console.log(error);
-    }
-}
-export default function LessonsList() {
-    useEffect(() => {
-        stopSpeech();
-      }, []);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const data = await fetchAllTopic();
-                
-                if (!data) return;
-                const sectionData: Section[] = data.map(item => ({
-                    topic: item.topic,
-                    chapter: item.chapter.map(chapter => ({
-                        id: chapter.id,
-                        name: chapter.name,
-                        description: chapter.description,
-                        topic_id: item.topic.id
-                    }))
-                }));
-                setSections(sectionData);
-                
-            } catch (error) {
-                console.log(error);
-            }
+export default function LessonsList({ navigation, route }: LessonListProps) {
+
+  const [sections, setSections] = useState<Section[]>([]);
+  const [listData, setListData] = useState<ListData[]>([]);
+
+  useEffect(() => {
+    stopSpeech();
+  }, []);
+
+  useEffect(() => {
+    if (route.params && route.params.chapters !== undefined) {
+      setListData([{ title: '', data: route.params.chapters }]);
+    } else {
+      const getData = async () => {
+        try {
+          const data = await fetchAllTopic();
+          if (!data) return;
+          const sectionData: Section[] = data.map(item => ({
+            topic: item.topic,
+            chapter: item.chapter.map(chapter => ({
+              id: chapter.id,
+              name: chapter.name,
+              description: chapter.description,
+              topic_id: item.topic.id
+            }))
+          }));
+          setSections(sectionData);
+
+        } catch (error) {
+          console.log(error);
         }
-        getData();
-    }, []);
+      }
+      getData();
+    }
+  }, []);
 
-    const [sections, setSections] = useState<Section[]>([]);
-    const [listData, setListData] = useState<ListData[]>([]);
+  useEffect(() => {
+    if (route.params && route.params.title) {
+      navigation.setOptions({ title: route.params.title });
+    }
+  }, []);
 
-    useEffect(() => {
-        if (sections.length === 0) return;
-        const listValue = sections.map((section: Section) => ({
-            title: section.topic.name,
-            data: section.chapter
-        }));
-        setListData(listValue);
-    }, [sections]);
+  useEffect(() => {
+    if (sections.length === 0) return;
+    const listValue = sections.map((section: Section) => ({
+      title: section.topic.name,
+      data: section.chapter
+    }));
+    setListData(listValue);
+  }, [sections]);
 
-    return (
-        <>
-         {listData && (<SectionList
-            sections={listData}
-            keyExtractor={(item, index) => item.id.toString()}
-            renderItem={({ item }) => ListCell({item: item, checked: true, style: {marginBottom: 16, marginHorizontal: 20}})}
-            renderSectionHeader={({ section: { title } }) => (
-                <Subtitle011 text={title} style={{paddingTop: 38, paddingBottom: 19, marginHorizontal: 20}} color={Colors.BLACK}/>
-            )}
-        />)}
-        </>
-    )
+  return (
+    <>
+      {listData && (<SectionList
+        sections={listData}
+        keyExtractor={(item, index) => item.id.toString()}
+        renderItem={({ item }) => ListCell({ item: item, checked: true, style: { marginBottom: 16, marginHorizontal: 20 } })}
+        renderSectionHeader={({ section: { title } }) => (
+          <>
+            {(title !== '') && ( <Subtitle011 text={title} style={{ paddingBottom: 12, marginHorizontal: 20 }} color={Colors.BLACK} /> )}
+          </>
+        )}
+        style={{ paddingTop: 16, paddingBottom: 100 }}
+      />)}
+    </>
+  )
 }
