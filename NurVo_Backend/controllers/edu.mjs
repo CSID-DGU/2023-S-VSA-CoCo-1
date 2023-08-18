b49c54183859446216bc02d1ec9795183cddbbd1
bshong12 <bshong3593@dgu.ac.kr>
2023-08-19 05:41:50 +0900

    [Feat] #18 이번 주 학습목표

--- NurVo_Backend/controllers/edu.mjs
+++ NurVo_Backend/controllers/edu.mjs
@@ -1,10 +1,11 @@
 import express from 'express';
-import {saveCompletedLearning, getCompletedChapter} from '../db/db.mjs';
+import {saveCompletedLearning, getCompletedChapter, getAllTodayLessons} from '../db/db.mjs';
 
 export const router = express.Router();
 
 router.post('/', saveEdu);  // 학습완료시 저장
 router.get('/review', getChapterForReview); // Review탭에서 불러올 학습완료 내역
+router.get('/todayLessons', getTodayLessons);
 
 async function saveEdu(req, res) {   // date, chapter_id, step 저장
   const data = req.body;
@@ -23,4 +24,11 @@ async function getChapterForReview(req, res) {
   const filterResult = result.filter(data => data.step == 3);  // 학습결과 step이 3인경우(학습완료)인 경우만 클라이언트로
 
   res.status(200).send(filterResult)
+}
+
+async function getTodayLessons(req, res) {
+  const id = req.user.id;
+
+  const result = await getAllTodayLessons(id)
+  res.status(200).send(result);
 }
