788e9fbd1d9b5cd2355985f1a2a303e6bc31bb47
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-08-22 00:22:01 +0900

    [Fix] #7 - 북마크 학습화면 오류 수정(원인 : parameter를 초과 작성)

--- NurVo_Frontend/src/components/StudyCard.tsx
+++ NurVo_Frontend/src/components/StudyCard.tsx
@@ -32,16 +32,16 @@ const StudyCard = ({ id, item, style, isAction }: StudyCardProps) => {
   const handleSpeak = async () => {
     if (!isSpeaking) {
       setIsSpeaking(true);
-      speech(item.dialogue, id, item.conversation_id, true, stopSpeech());
+      speech(item.dialogue, item.conversation_id, true, () => { setIsSpeaking(false) });
     }
   }
 
-  useEffect(()=>{
-    if (isAction === 'remove' || isAction === 'keep'){
+  useEffect(() => {
+    if (isAction === 'remove' || isAction === 'keep') {
       stopSpeech();
       setIsSpeaking(false);
     }
-  },[isAction]);
+  }, [isAction]);
 
   return (
     <TouchableOpacity onPress={turnCard} style={styles.container}>
